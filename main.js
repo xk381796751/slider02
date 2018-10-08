e5f43581a343a531232fa21ab96eab5a13dc0c22
Keller <381796751@qq.com>
2018-10-08 17:30:26 +0800

    第一个版本

---
+++ main.js
@@ -0,0 +1,64 @@
+let n
+init()
+setInterval(()=>{
+  makeLeave(getImage(n))
+    .one('transitionend', (e)=>{
+      makeEnter($(e.currentTarget))
+    })
+  makeCurrent(getImage(n+1))
+  n += 1
+},2000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 下面可以不看
+
+function getImage(n){
+  return $(`.images > img:nth-child(${x(n)})`)
+}
+
+function x(n){
+  if(n>3){
+    n = n%3
+    if (n===0){
+      n =3
+    }
+  } // n = 1 2 3
+  return n
+}
+
+function init(){
+  n = 1
+  $(`.images > img:nth-child(${n})`).addClass('current')
+    .siblings().addClass('enter')
+}
+
+function makeCurrent($node){
+  return $node.removeClass('enter leave').addClass('current')
+}
+function makeLeave($node){
+  return $node.removeClass('enter current').addClass('leave')
+}
+function makeEnter($node){
+  return $node.removeClass('leave current').addClass('enter')
+}
