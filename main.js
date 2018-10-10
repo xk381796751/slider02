fa36276c497d25e3fd6c9a99eb637f6e6f8b6c4b
Keller <381796751@qq.com>
2018-10-10 10:56:34 +0800

    解决bug

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
 let n
 init()
-setInterval(()=>{
-  makeLeave(getImage(n))
-    .one('transitionend', (e)=>{
-      makeEnter($(e.currentTarget))
-    })
-  makeCurrent(getImage(n+1))
-  n += 1
-},2000)
+let timer = setInterval(() => {
+	makeLeave(getImage(n))
+		.one('transitionend', (e) => {
+			makeEnter($(e.currentTarget))
+		})
+	makeCurrent(getImage(n + 1))
+	n += 1
+}, 2000)
+
+document.addEventListener('visibilitychange', function(e) {
+	if (document.hidden) {
+		window.clearInterval(timer)
+	} else {
+		timer = setInterval(() => {
+			makeLeave(getImage(n))
+				.one('transitionend', (e) => {
+					makeEnter($(e.currentTarget))
+				})
+			makeCurrent(getImage(n + 1))
+			n += 1
+		}, 2000)
+	}
+})
 
 
 
@@ -33,32 +48,32 @@ setInterval(()=>{
 
 // 下面可以不看
 
-function getImage(n){
-  return $(`.images > img:nth-child(${x(n)})`)
+function getImage(n) {
+	return $(`.images > img:nth-child(${x(n)})`)
 }
 
-function x(n){
-  if(n>3){
-    n = n%3
-    if (n===0){
-      n =3
-    }
-  } // n = 1 2 3
-  return n
+function x(n) {
+	if (n > 3) {
+		n = n % 3
+		if (n === 0) {
+			n = 3
+		}
+	} // n = 1 2 3
+	return n
 }
 
-function init(){
-  n = 1
-  $(`.images > img:nth-child(${n})`).addClass('current')
-    .siblings().addClass('enter')
+function init() {
+	n = 1
+	$(`.images > img:nth-child(${n})`).addClass('current')
+		.siblings().addClass('enter')
 }
 
-function makeCurrent($node){
-  return $node.removeClass('enter leave').addClass('current')
+function makeCurrent($node) {
+	return $node.removeClass('enter leave').addClass('current')
 }
-function makeLeave($node){
-  return $node.removeClass('enter current').addClass('leave')
+function makeLeave($node) {
+	return $node.removeClass('enter current').addClass('leave')
 }
-function makeEnter($node){
-  return $node.removeClass('leave current').addClass('enter')
+function makeEnter($node) {
+	return $node.removeClass('leave current').addClass('enter')
 }
